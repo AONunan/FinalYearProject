0052a67cfc95b5bcbfa312d882076d79a8673e87
Alan Noonan <aganoonan@gmail.com>
2017-01-25 15:56:21 +0000

    Added functions and private variables to PacketCaptureWindow

--- packetcapturewindow.cpp
+++ packetcapturewindow.cpp
@@ -12,3 +12,28 @@ PacketCaptureWindow::~PacketCaptureWindow()
 {
     delete ui;
 }
+
+void PacketCaptureWindow::on_button_applyFilter_clicked()
+{
+
+}
+
+void PacketCaptureWindow::on_button_close_handle_clicked()
+{
+
+}
+
+void PacketCaptureWindow::on_button_capture_packet_clicked()
+{
+
+}
+
+void PacketCaptureWindow::on_pushButton_test_clicked()
+{
+
+}
+
+void PacketCaptureWindow::on_button_capture_stream_clicked()
+{
+
+}
--- packetcapturewindow.h
+++ packetcapturewindow.h
@@ -2,6 +2,7 @@
 #define PACKETCAPTUREWINDOW_H
 
 #include <QDialog>
+#include <pcap.h>
 
 namespace Ui {
 class PacketCaptureWindow;
@@ -15,8 +16,29 @@ public:
     explicit PacketCaptureWindow(QWidget *parent = 0);
     ~PacketCaptureWindow();
 
+    void testFunction();
+
+
+
+private slots:
+    void on_button_applyFilter_clicked();
+    void on_button_close_handle_clicked();
+    void on_button_capture_packet_clicked();
+    void on_pushButton_test_clicked();
+    void on_button_capture_stream_clicked();
+
 private:
     Ui::PacketCaptureWindow *ui;
+
+    char *dev;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle;                     // Session handle
+    struct bpf_program fp;              // The compiled filter expression
+    //char filter_exp[] = "port 443";	// The filter expression
+    bpf_u_int32 mask;                   // The netmask of our sniffing device
+    bpf_u_int32 net;                    // The IP of our sniffing device
+    struct pcap_pkthdr header;          // The header that pcap gives us
+    const u_char *packet;               // The actual packet
 };
 
 #endif // PACKETCAPTUREWINDOW_H
