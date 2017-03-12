b75d6bda47fc04a7a4d481ed6b45f389b5893968
Alan Noonan <aganoonan@gmail.com>
2017-03-12 17:52:38 +0000

    Added TCP Tahoe simulation

--- congestionwindow.cpp
+++ congestionwindow.cpp
@@ -1,5 +1,6 @@
 #include "congestionwindow.h"
 #include "ui_congestionwindow.h"
+#include <math.h> // For floor()
 
 CongestionWindow::CongestionWindow(QWidget *parent) :
     QDialog(parent),
@@ -11,6 +12,15 @@ CongestionWindow::CongestionWindow(QWidget *parent) :
 }
 
 void CongestionWindow::reset_variables() {
+    ui->pushButton_send->setEnabled(true);
+    ui->pushButton_clear->setEnabled(false);
+    ui->pushButton_ack->setEnabled(false);
+    ui->pushButton_drop->setEnabled(false);
+    ui->textBrowser_tahoe_variables->setText("");
+    ui->textBrowser_tahoe_action->setText("");
+    ui->textBrowser_reno_variables->setText("");
+    ui->textBrowser_reno_action->setText("");
+
     tahoe_data.clear();
     reno_data.clear();
 
@@ -24,6 +34,9 @@ void CongestionWindow::reset_variables() {
 
     x_axis = 1;
     tahoe_mss = 1;
+    tahoe_cwnd = tahoe_mss;
+    tahoe_slow_start = true; // Begin in Slow Start state
+    tahoe_ssthresh = 20;
 }
 
 void CongestionWindow::draw_plots() {
@@ -51,6 +64,12 @@ void CongestionWindow::draw_plots() {
     reno_chart->addSeries(series_reno);
     reno_chart->createDefaultAxes();
     ui->chart_widget_reno->setChart(reno_chart);
+
+    QString tahoe_var_details = QString("Congestion Window = %1\n"
+                                        "Max Segment Size = %2\n"
+                                        "Slow Start threshold = %3\n"
+                                        "Strategy = %4").arg(tahoe_cwnd).arg(tahoe_mss).arg(tahoe_ssthresh).arg((tahoe_slow_start) ? "Slow Start" : "Congestion Avoidance");
+    ui->textBrowser_tahoe_variables->setText(tahoe_var_details);
 }
 
 CongestionWindow::~CongestionWindow()
@@ -60,8 +79,25 @@ CongestionWindow::~CongestionWindow()
 
 void CongestionWindow::on_pushButton_send_clicked()
 {
+    QString tahoe_explanation;
+
+    if(tahoe_slow_start) {
+        tahoe_explanation = QString("We are in Slow Start mode, as the Congestion Window is less than/equal to the Slow Start Threshold (%1 <= %2). We see an exponential increase in the Congestion Window.\n\n").arg(tahoe_cwnd).arg(tahoe_ssthresh);
+
+    } else {
+        tahoe_explanation = QString("We are in Congestion Avoidance mode, as the Congestion Window is greater than the Slow Start Threshold (%1 > %2). We see a linear increase in the Congestion Window.\n\n").arg(tahoe_cwnd).arg(tahoe_ssthresh);
+    }
+
+    tahoe_explanation += QString("We send %1 data.").arg(tahoe_cwnd);
+
+    ui->textBrowser_tahoe_action->setText(tahoe_explanation);
+
     update_tahoe_points();
     update_reno_points();
+
+    ui->pushButton_send->setEnabled(false);
+    ui->pushButton_ack->setEnabled(true);
+    ui->pushButton_drop->setEnabled(true);
 
     draw_plots();
     x_axis++;
@@ -70,10 +106,10 @@ void CongestionWindow::on_pushButton_send_clicked()
 void CongestionWindow::update_tahoe_points() {
     QVector<int> row_tahoe;
     row_tahoe.append(x_axis); // x-axis
-    row_tahoe.append(tahoe_mss); // y-axis
+    row_tahoe.append(tahoe_cwnd); // y-axis
     tahoe_data.append(row_tahoe);
 
-    tahoe_mss *= 2; // Double the MSS
+
 }
 
 void CongestionWindow::update_reno_points() {
@@ -85,12 +121,59 @@ void CongestionWindow::update_reno_points() {
 
 void CongestionWindow::on_pushButton_ack_clicked()
 {
+    QString tahoe_explanation;
 
+    tahoe_explanation += "Successful acknowledgement of data.\n\n";
+
+    if(tahoe_slow_start) {
+        tahoe_explanation += QString("We are in Slow Start mode.\n"
+                                     "The Congestion Window is increased by the value of the Max Segment Size (%1 + %2 = %3).\n"
+                                     "The Max Segment Size doubles (%2 to %4).\n\n").arg(tahoe_cwnd).arg(tahoe_mss).arg(tahoe_cwnd + tahoe_mss).arg(2 * tahoe_mss);
+
+        tahoe_cwnd += tahoe_mss;
+        tahoe_mss *= 2; // Double the MSS
+
+        if(tahoe_cwnd > tahoe_ssthresh) {
+            tahoe_explanation += QString("The Congestion Window is now greater than the Slow start Threshold (%1 > %2), therefore we switch to Congestion Avoidance.").arg(tahoe_cwnd).arg(tahoe_ssthresh);
+            tahoe_slow_start = false; // Switch to congestion avoidance
+        } else {
+            tahoe_explanation += QString("The congestion window is still less than/equal to than the slow start threshold (%1 <= %2), therefore we remain in Slow Start.").arg(tahoe_cwnd).arg(tahoe_ssthresh);
+        }
+
+    } else { // Congestion avoidance
+        tahoe_explanation += QString("We are in Congestion Avoidance mode.\n"
+                                     "Congestion Window += (MSS^2) / cwnd\n"
+                                     "Congestion Window += %1^2 / %2\n"
+                                     "Congestion Window = %3").arg(tahoe_mss).arg(tahoe_cwnd).arg(floor(tahoe_cwnd + (pow(tahoe_mss, 2) / tahoe_cwnd)));
+        tahoe_cwnd += pow(tahoe_mss, 2) / tahoe_cwnd; // cwnd += mss^2 / cwnd
+    }
+
+    ui->textBrowser_tahoe_action->setText(tahoe_explanation);
+
+    ui->pushButton_send->setEnabled(true);
+    ui->pushButton_ack->setEnabled(false);
+    ui->pushButton_drop->setEnabled(false);
 }
 
 void CongestionWindow::on_pushButton_drop_clicked()
 {
+    QString tahoe_explanation;
 
+    tahoe_explanation = QString("A packet has been dropped.\n\n"
+                        "Set the new Slow Start threshold to half the current congestion window (%1 / 2 = %2).\n"
+                        "Reset the congestion window back to 1.\n"
+                        "Return to Slow Start mode.").arg(tahoe_cwnd).arg(tahoe_cwnd / 2);
+
+    ui->textBrowser_tahoe_action->setText(tahoe_explanation);
+
+    tahoe_ssthresh = tahoe_cwnd / 2; // Divide c
+    tahoe_mss = 1;
+    tahoe_cwnd = 1;
+    tahoe_slow_start = true;
+
+    ui->pushButton_send->setEnabled(true);
+    ui->pushButton_ack->setEnabled(false);
+    ui->pushButton_drop->setEnabled(false);
 }
 
 void CongestionWindow::on_pushButton_clear_clicked()
